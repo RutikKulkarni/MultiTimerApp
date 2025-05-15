1ad491fd91a31680e26042f69e62f2b28e2fa2b9
Rutik <rutikkulkarni2001@gmail.com>
2025-05-15 18:14:39 +0530

    Init

--- src/services/exportService.tsx
+++
@@ -1,6 +0,0 @@
-import {HistoryEntry} from '../hooks/useTimer';
-
-export function exportHistory(history: HistoryEntry[]) {
-  const data = JSON.stringify(history, null, 2);
-  console.log('Exported history:', data);
-}
--- src/styles/theme.tsx
+++
@@ -1,6 +0,0 @@
-import {colors} from './colors';
-
-export type Theme = typeof colors.light;
-
-export const lightTheme: Theme = colors.light;
-export const darkTheme: Theme = colors.dark;
